3093c6136819246e34049c93e30627c36ad86818
Keryn Knight <keryn@kerynknight.com>
2015-02-28 10:53:33 +0000

    be compatible

--- blanket.py
+++ blanket.py
@@ -1,4 +1,9 @@
-from collections import OrderedDict
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from collections import OrderedDict, namedtuple
 import logging
 from webob import Request
 from webob import Response
